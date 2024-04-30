711fd69f3eda564a098f3087b3df34b01c31194e
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-04-30 23:54:35 +0100

    Commit 1 Footer JS

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <section className="copyright">
+          <p>&copy; James Hutchinson 2024</p>
+        </section>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
