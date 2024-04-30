9e597cc6eb2e5e9ec502f29f88fcef2acb2ee602
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-04-30 23:42:17 +0100

    Commit 2 Header JS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
       <section className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
       </section>
+      <h1 className="h1-title">James Hutchinson - Front-End Web Design and Developer</h1>
     </body>
   );
 };
