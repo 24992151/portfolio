b187213bbc444a9360018b91e41b42ea2354a53b
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-05-02 18:34:08 +0100

    Commit 3 Links Added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 import logo from '../../assets/JHLogo.jpg';
 
 const Header = () => {
   return (
-    <body className="header">
+    <header className="header">
       <section className="logo-container">
-        <img src={logo} alt="Logo" className="logo" />
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
       </section>
-      <h1 className="h1-title">James Hutchinson - Front-End Web Design and Developer</h1>
-    </body>
+      <h1 className="h1-title">James Hutchinson - Front-End Web Design and Development</h1>
+    </header>
   );
 };
 
