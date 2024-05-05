641e60458b1e9a6650f5afc8bac5a03e88bd441b
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-05-05 03:33:12 +0100

    Commit 2 JDM Page JS
    Finished and Commented JS

--- src/pages/Jdm/JdmPage.js
+++ src/pages/Jdm/JdmPage.js
@@ -2,10 +2,19 @@
 import React from 'react';
 import '../projects.css';
 import jdmvideo from '../../assets/jdmvideo.mp4';
+import bglive from '../../assets/bglive.mp4';
 
 function JdmPage() {
   return (
     <div className="page">
+
+      {/* Live Background */}
+      <section>
+        <video autoPlay muted loop id="bglive">
+          <source src={bglive} type="video/mp4" />
+        </video>
+      </section>
+
       {/* Video */}
       <section className="video">
         <video autoPlay loop muted>
@@ -18,6 +27,7 @@ function JdmPage() {
         <section className="projects-container">
           <section className="practices">
             <h2 className="title">Practices</h2>
+            <p>In this project, I didn't use a framework due to the fact it was primarily focused on the animation and different styling of website than found in ubiquitous web design. It has a combination of CSS animation, unconventional navigation and layout and unique interactions.</p>
           </section>
           <section className="links">
             <h2 className="title">Links</h2>
@@ -25,6 +35,7 @@ function JdmPage() {
           </section>
           <section className="included">
             <h2 className="title">What's Included?</h2>
+            <p>As this was a learning experience in more advanced animations and interactions. I mainly tested using the likes of css animations on hoverable effects and keyframe animation. On top of that, the use of interaction helped create a narrative-like experience for users. The entire point of this was going against traditional elements mainly found on sites and then creating a working build for desktop which turned out a success in the post-build user research.</p>
           </section>
         </section>
       </article>
